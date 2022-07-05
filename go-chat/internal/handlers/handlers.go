42a9238b04d41e5293c17d2c0205c072e850aff1
Hyohwak Lee <crewe1746@naver.com>
2022-07-05 10:34:56 +0900

    Create a handler that upgrades connection to websocket

--- go-chat/internal/handlers/handlers.go
+++ go-chat/internal/handlers/handlers.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/CloudyKit/jet/v6"
+	"github.com/gorilla/websocket"
 )
 
 var views = jet.NewSet(
@@ -30,4 +31,35 @@ func renderPage(w http.ResponseWriter, tmpl string, data jet.VarMap) error {
 	}
 
 	return nil
+}
+
+var upgradeConnection = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+// define the response sent back from websocket
+type WsJsonResponse struct {
+	Action      string `json:"action"`
+	Message     string `json"message"`
+	MessageType string `json"message_type"`
+}
+
+// upgrade connection to websocket
+func WsEndpoint(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgradeConnection.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Client connected to websocket")
+
+	var response WsJsonResponse
+	response.Message = `<em><small>Connected to server</small></em>`
+
+	err = ws.WriteJSON(response)
+	if err != nil {
+		log.Println(err)
+	}
 }
